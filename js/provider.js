b8f10ab48119db33026b391b055d4b49d31cd191
ilovezy <1518265554@qq.com>
2015-11-19 12:57:42 +0800

    a lot of shapes

---
+++ js/provider.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: fhc
+ * @Date:   2015-11-19 09:19:07
+ * @Last Modified by:   fhc
+ * @Last Modified time: 2015-11-19 09:52:43
+ */
+
+app.controller('mainController', ['$scope', '$http',
+    function($scope, $http) {
+        $http.get()
+            .then();
+    }
+]);
